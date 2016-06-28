aa34c2f14e56d68cb25c933551c05df4e975fcc9
Julio <juliofornode@gmail.com>
2016-06-28 09:43:17 -0700

    setting mongoose

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 var express = require('express');
+var logger = require('morgan');
 var router = require('./server/routes');
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/todo023', function(error) {
+    if(error) {
+        console.log('error connecting database');
+    } else {
+        console.log('connected to database');
+    }
+});
+
+var taskSchema = new mongoose.Schema({
+    task: String
+});
+
+var Task = mongoose.model('Task', taskSchema);
+
+var taskOne = new Task({
+    task: "lavar el coche"
+});
+
+taskOne.save();
 
 var app = express();
 
 
 app.use(express.static('client'));
 app.use('/api', router);
+app.use(logger('combined'));
+
 
 
 
